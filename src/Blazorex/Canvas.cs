3dcc70362ee048d1250e5665fb514c3282927eec
David Guida <davidguida@microsoft.com>
2022-09-18 22:00:23 -0400

    minor fixes

--- src/Blazorex/Canvas.cs
+++ src/Blazorex/Canvas.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
 
 namespace Blazorex
 {
