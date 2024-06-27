2f2b3a58cdc0c8dd6473cbab7a9ef6f43aff6117
David Guida <1432872+mizrael@users.noreply.github.com>
2024-06-27 13:42:08 -0400

    minor fixes

--- src/Blazorex/CanvasCreationOptions.cs
+++ src/Blazorex/CanvasCreationOptions.cs
@@ -1,22 +1,41 @@
 ﻿using Microsoft.AspNetCore.Components;
 using System;
 
-namespace Blazorex
+namespace Blazorex;
+
+public readonly struct CanvasCreationOptions
 {
-    public readonly struct CanvasCreationOptions
-    {
-        public ElementReference ElementReference { get; init; }
-        public required int Width { get; init; }
-        public required int Height { get; init; }
+    public ElementReference ElementReference { get; init; }
+    
+    /// <summary>
+    /// the width of the canvas
+    /// </summary>
+    public required int Width { get; init; }
 
-        public bool Hidden { get; init; }
+    /// <summary>
+    /// the height of the canvas
+    /// </summary>
+    public required int Height { get; init; }
 
-        public Action<CanvasBase> OnCanvasReady { get; init; }
-        public Action<float> OnFrameReady { get; init; }
+    /// <summary>
+    /// when true, the canvas will not be rendered, but will keep triggering events
+    /// </summary>
+    public bool Hidden { get; init; }
 
-        public Action<int> OnKeyUp { get; init; }
-        public Action<int> OnKeyDown { get; init; }
-        public Action<MouseCoords> OnMouseMove { get; init; }
-        public Action<Size> OnResize { get; init; }
-    }
+    /// <summary>
+    /// fired when the canvas is ready to process events
+    /// </summary>
+    public Action<CanvasBase> OnCanvasReady { get; init; }
+
+    /// <summary>
+    /// fired at every frame refresh
+    /// </summary>
+    public Action<float> OnFrameReady { get; init; }
+
+    public Action<int> OnKeyUp { get; init; }
+    public Action<int> OnKeyDown { get; init; }
+
+
+    public Action<MouseCoords> OnMouseMove { get; init; }
+    public Action<Size> OnResize { get; init; }
 }
