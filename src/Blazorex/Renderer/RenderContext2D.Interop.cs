81b4e78a847ff964c07e93316f34970603b795cf
zettersten <erik@veefriends.com>
2025-06-06 14:56:18 -0400

    adds missing this (missed a spot)

--- src/Blazorex/Renderer/RenderContext2D.Interop.cs
+++ src/Blazorex/Renderer/RenderContext2D.Interop.cs
@@ -6,11 +6,11 @@ namespace Blazorex.Renderer;
 internal sealed partial class RenderContext2D : IRenderContext
 {
     internal async ValueTask<T> DirectCall<T>(string method, params object[] args) =>
-        await _blazorexAPI.InvokeAsync<T>("directCall", _id, method, args);
+        await this._blazorexAPI.InvokeAsync<T>("directCall", _id, method, args);
 
     async ValueTask IRenderContext.ProcessBatchAsync()
     {
-        await _blazorexAPI.InvokeVoidAsync("processBatch", _id, _jsOps);
-        _jsOps.Clear();
+        await this._blazorexAPI.InvokeVoidAsync("processBatch", _id, _jsOps);
+        this._jsOps.Clear();
     }
 }
